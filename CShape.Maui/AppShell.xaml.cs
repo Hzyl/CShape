e23eccb864a287cf50f4a668a2621f35a10b7bd9
Hzyl <phulonghuy73@gmail.com>
2026-03-29 10:36:21 +0700

    delete

--- CShape.Maui/AppShell.xaml.cs
+++
@@ -1,9 +0,0 @@
-namespace CShape.Maui;
-
-public partial class AppShell : Shell
-{
-    public AppShell()
-    {
-        InitializeComponent();
-    }
-}
--- CShape.Shared/Class1.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace CShape.Shared;
-
-public class Class1
-{
-
-}
